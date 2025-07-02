efc70562ed048694e7549b8954405be8e446f096
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-07-02 17:05:11 +0900

    Header,Footer コンポーネントの調整

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Haruto Yamane's Portfolio";
+export const SITE_DESCRIPTION = '山根 晴斗のポートフォリオサイトです。';
