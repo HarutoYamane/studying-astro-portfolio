6da9a653ac9876286a28ae1bc1a1077ced97376c
Yamane Haruto <haru.momo.coco.happy@gmail.com>
2025-07-05 15:07:43 +0900

    BASE_URLを新しく定義

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,4 @@
 export const SITE_TITLE = "Haruto Yamane's Portfolio";
 export const SITE_DESCRIPTION = '山根 晴斗のポートフォリオサイトです。';
 export const JOB_TITLE = 'Webエンジニア初学者';
+export const BASE_URL = import.meta.env.BASE_URL;
